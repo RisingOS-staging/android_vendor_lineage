1686386f5f92d5d84c03a6aab45e4d3f1d3ec158
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-08 16:03:13 +0800

    [RisingOS] Version control changes

--- config/common.mk
+++ config/common.mk
@@ -2,7 +2,7 @@
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 include vendor/rising/config/rising.mk
 
-PRODUCT_BRAND ?= LineageOS
+PRODUCT_BRAND ?= risingOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
